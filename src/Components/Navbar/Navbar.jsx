bd1eaff19804fa6576752f781523ce157f5acab2
Federico E Santos <boomarts@yahoo.com>
2024-02-16 13:14:27 -0300

    comenzando nuevo proyecto con boom arts

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Navbar = () => {
+
+
+    return (
+        <div className="navbar">
+            <img src="./img/logo-boom-arts.png" alt="logo" className='img-nav'/>
+            <div className='nav'>
+                <nav>
+
+                </nav>
+                <p>Bienvenidos</p>
+            </div>
+            <div className='carrito'>
+                <img src="./img/carrito.png" alt="carr" />
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
