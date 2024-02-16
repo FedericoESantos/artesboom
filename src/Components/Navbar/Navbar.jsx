564853ed9b43190c6d199655f18a0f476ebed581
Federico E Santos <boomarts@yahoo.com>
2024-02-16 13:34:33 -0300

    comenzando nuevo proyecto con boom arts 2

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -8,7 +8,16 @@ const Navbar = () => {
             <img src="./img/logo-boom-arts.png" alt="logo" className='img-nav'/>
             <div className='nav'>
                 <nav>
-
+                    <ul>
+                        <li>
+                            <a href="">Inicio</a>
+                            <a href="">Quienes Somos</a>
+                            <a href="">Manualidades</a>
+                            <a href="">Cocina</a>
+                            <a href="">Ocio y Juegos</a>
+                            <a href="">Contacto</a>
+                        </li>
+                    </ul>
                 </nav>
                 <p>Bienvenidos</p>
             </div>
