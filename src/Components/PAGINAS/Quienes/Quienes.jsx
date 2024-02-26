cea762befb185698415fc07fe453c7f02ee501e0
Federico E Santos <boomarts@yahoo.com>
2024-02-26 12:23:09 -0300

    proyecto de boom arts - relleno de paginas parte 1

--- src/Components/PAGINAS/Quienes/Quienes.jsx
+++ src/Components/PAGINAS/Quienes/Quienes.jsx
@@ -3,6 +3,64 @@ import React from 'react'
 const Quienes = () => {
     return (
         <div className='quienes'>
+            <div className="titulo">
+                <h1>¿ Quienes Somos ?</h1>
+            </div>
+            <div className="content-1">
+                <div>
+                <img src="./img/fotos/amira.jpg" alt="foto-danza" />
+                </div>
+                <div>
+                <p>Boom Arts es una empresa artística en crecimiento, dedicada a brindarle la mayor prestación en servicios </p>
+                <p>Gráficos, Fotográficos, Publicidad, Manualidades y diversas índoles artísticas.</p>
+                <p>Boom Arts comenzó como una idea, por el año 2010, con un fin artístico </p>
+                <p>de brindar servicios Fotogáficos y Videográficos. Luego tras los años</p>
+                <p> se ha logrado expandir e incorporar nuevas ideas, nuevos conocimientos fueron surgiendo </p>
+                <p>trayendo nuevas metas cumplidas, y muchas más por cumplir año tras año.</p>
+                </div>
+                <div>
+                <img src="./img/fotos/frozen.jpg" alt="foto-castillo" />
+                </div>
+            </div>
+
+            <div className="content-1">
+                <div>
+                <img src="./img/fotos/triptico.jpg" alt="cuadros" />
+                </div>
+                <div>
+                <h2>Diseño Gráfico </h2>
+                <p>Años de experiencia marcan nuestra trayectoria, manejando las mejores herramientas </p>
+                <p>gráficas para crear imágenes digitales con diversos efectos, </p>
+                <p>que atenúan la belleza de un excelente trabajo terminado. </p>
+                <p>Desde trabajos digitales hasta trabajos físicos, manejando un target publicitario</p>
+                <p> de diversos negocios. Colocación de cartelerías. Fotomontajes digitales. Baner publicitarios</p>
+                <p>Y proximamente sumamos diseños de páginas web personalizadas o comerciales.</p>
+                </div>
+                <div>
+                <img src="./img/fotos/lona.jpg" alt="lona" className='lona' />
+                </div>
+            </div>
+
+            <div className="content-1">
+                <div>
+                <img src="./img/fotos/tarjeta.jpg" alt="tarjeta" className='tarjeta'/>
+                </div>
+                <div>
+                <h2>Creatividad </h2>
+                <p>Todos los días trabajamos para inventar todo tipo de arte, con ideas tan locas como originales,</p>
+                <p> ampliando el terreno de la imaginación, para demostrarles que amplio puede ser el mundo</p>
+                <p> de la creatividad. Existe un lema en todo ser artístico que dice </p>
+                <p>si lo puedes imaginar, lo puedes crear. Y aquí estamos, para satisfacer sueños.</p>
+                </div>
+                <div>
+                <img src="./img/fotos/flyer.jpg" alt="lona" className='flyer' />
+                </div>
+            </div>
+
+            <div>
+            <img src="./img/fotos/viejo-logo.png" alt="viejo-logo" className='viejo-logo'/>
+            </div>
+            
 
         </div>
     )
